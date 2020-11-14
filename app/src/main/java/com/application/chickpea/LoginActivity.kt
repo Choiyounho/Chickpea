d55610b3de2db5563080b55b4adabfc99610a3d3
oueya1479 <68142821+oueya1479@users.noreply.github.com>
2020-11-14 14:54:34 +0900

    change login

---
+++ app/src/main/java/com/application/chickpea/LoginActivity.kt
@@ -0,0 +1,81 @@
+package com.application.chickpea
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_login.*
+
+
+class LoginActivity : AppCompatActivity() {
+
+    // Google Sign-In Methods
+    var googleSignInClient : GoogleSignInClient? = null
+    val RC_SIGN_IN = 1000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // Google Sign-In Methods
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        btn_google.setOnClickListener {
+            var signInIntent = googleSignInClient?.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
+    }
+
+    // Move to MainActivity
+    fun fun_MoveNextPage(){
+        var currentUser = FirebaseAuth.getInstance().currentUser
+        if(currentUser != null){
+            startActivity(Intent(this, MainActivity::class.java))
+            this.finish()
+        }
+    }
+
+    // Google Sign-In Methods
+    fun fun_FirebaseAuthWithGoogle(acct : GoogleSignInAccount?){
+        var credential = GoogleAuthProvider.getCredential(acct?.idToken, null)
+        FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener { task ->
+            if(task.isSuccessful){
+                fun_MoveNextPage()
+            }
+        }
+    }
+
+    //Override
+    override fun onResume() {
+        super.onResume()
+        fun_MoveNextPage()  //Automatic Login
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Google Sign-In Methods
+        if(requestCode == RC_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try{
+                val account = task.getResult(ApiException::class.java)
+                fun_FirebaseAuthWithGoogle(account)
+            }
+            catch (e : ApiException){
+
+            }
+        }
+        else{
+
+        }
+    }
+}
